68c73579e457c954eceb408a95a53658fb7d749e
jesseleeruiz <jesseleeruiz@gmail.com>
2020-08-22 15:25:48 -1000

    Added VC with buttons, etc. Redirected Focus Guide to textfield. Added alert to Next Button.

--- Focus Technique/Focus Technique/ViewController.swift
+++ Focus Technique/Focus Technique/ViewController.swift
@@ -9,12 +9,54 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // MARK: - Outlets
+    @IBOutlet var textfield: UITextField!
+    @IBOutlet var textFieldTrip: UILabel!
+    @IBOutlet var nextButton: UIButton!
+    
+    // MARK: - Properties
+    var focusGuide: UIFocusGuide!
+    
+    // Change the start Focus Guide to somewhere else
+    override var preferredFocusEnvironments: [UIFocusEnvironment] {
+        return [textfield]
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Configure the Focus Guide
+        focusGuide = UIFocusGuide()
+        view.addLayoutGuide(focusGuide)
+        
+        // Place the Focus Guide via Anchors
+        focusGuide.topAnchor.constraint(equalTo: textfield.bottomAnchor).isActive = true
+        focusGuide.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+        focusGuide.heightAnchor.constraint(equalToConstant: 1).isActive = true
+        
+        focusGuide.preferredFocusEnvironments = [nextButton]
     }
-
+    
+    // MARK: - Actions
+    @IBAction func showAlert(_ sender: UIButton) {
+        let alertController = UIAlertController(title: "Hello", message: nil, preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        present(alertController, animated: true)
+    }
+    
+    // MARK: - Methods
+    override func didUpdateFocus(in context: UIFocusUpdateContext, with coordinator: UIFocusAnimationCoordinator) {
+        super.didUpdateFocus(in: context, with: coordinator)
+        // If the user is moving towards the text field
+        if context.nextFocusedView == textfield {
+            // Tell the focus guide to redirect to the next button
+            focusGuide.preferredFocusEnvironments = [nextButton]
+        } else if context.nextFocusedView == nextButton {
+            // Otherwise tell the focus guide to redirect to the text field
+            focusGuide.preferredFocusEnvironments = [textfield]
+        }
+    }
+    
 
 }
 
