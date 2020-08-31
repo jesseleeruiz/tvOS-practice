fd9ea25b6d7104503471e956cec5ec297a3a1909
jesseleeruiz <jesseleeruiz@gmail.com>
2020-08-31 11:07:31 -1000

    Completed project Dark Mode.

--- Dark Mode/Dark Mode/ViewController.swift
+++ Dark Mode/Dark Mode/ViewController.swift
@@ -12,9 +12,20 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let light = UITraitCollection(userInterfaceStyle: .light)
+        let dark = UITraitCollection(userInterfaceStyle: .dark)
+        
+        UISegmentedControl.appearance(for: light).tintColor = .blue
+        UISegmentedControl.appearance(for: dark).tintColor = .red
     }
-
-
+    
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        if traitCollection.userInterfaceStyle == .dark {
+            view.backgroundColor = UIColor(red: 0.3, green: 0, blue: 0, alpha: 1)
+        } else {
+            view.backgroundColor = UIColor(red: 0.7, green: 0.7, blue: 1, alpha: 1)
+        }
+    }
 }
 
